1c79d1eb1c8e0c996ad587c99ea9a03b2d8bca18
DELL <DELL@DESKTOP-7NCQDFI>
2023-05-09 19:43:34 +0500

    Add project files.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.dta;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connect = builder.Configuration.GetConnectionString("defaultString");
+builder.Services.AddDbContext<DbDataContext>(options => options.UseSqlServer(connect));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ WebApplication1/dta/DbDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Model;
+
+namespace WebApplication1.dta
+{
+    public class DbDataContext : DbContext
+    {
+        public DbDataContext(DbContextOptions<DbDataContext> options) : base(options) { 
+        
+        }
+
+        public DbSet<Product> products { get; set; }
+    }
+}
